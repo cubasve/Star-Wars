7a28ce7ed9760b618c17a065e0d5d8128ffbe3b3
cubasve <eva.cubasvasquez@gmail.com>
2021-01-04 22:11:26 -0500

    Add loading message in case of delays

--- src/App.js
+++ src/App.js
@@ -7,30 +7,47 @@ import { getAllStarships } from './services/sw-api';
 export default class App extends Component {
   state = {
     starships: [],
+    loading: true,
+    errorMessage: '',
   }
 
   async componentDidMount() {
     try {
       const starshipData = await getAllStarships();
       console.log(starshipData);
-      this.setState({ starships: starshipData.results });
+      this.setState({
+        starships: starshipData.results,
+        loading: false,
+      });
     } catch (err) {
       console.error(err);
+      this.setState({
+        errorMessage: 'There was an error!',
+        loading: false,
+      });
     }
   }
 
   render() {
+
+    if (this.state.loading) {
+      return <h1>Loading...</h1>
+    }
+
+    if (this.state.errorMessage) {
+      return <h1>{this.state.errorMessage}</h1>
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <Switch>
-            <Route exact path="/">
-              {this.state.starships.map((starship, id) => (<Link to={`/starship/${id}`}>{starship.name}</Link>))}
-            </Route>
+        <header className="App-header">STAR WARS STARSHIPS</header>
+        <Switch>
+          <Route exact path="/">
+            {this.state.starships.map((starship, id) => (<Link to={`/starship/${id}`}>{starship.name}</Link>))}
+          </Route>
 
-            <Route path='/starship/:id' render={props => <StarshipPage {...props} ship={this.state.starships} />} />
-          </Switch>
-        </header>
+          <Route path='/starship/:id' render={props => <StarshipPage {...props} ship={this.state.starships} />} />
+        </Switch>
       </div>
     )
   }
